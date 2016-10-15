f8d6206ed9957dba7d705ead3c70444474722f1b
Kisiev <kisivaleri@gmail.com>
2016-10-15 16:28:33 +0300

    создал базу данный и занес несколько записей

---
+++ app/src/main/java/list/umorili/android/com/umorili/UmoriliAplication.java
@@ -0,0 +1,15 @@
+package list.umorili.android.com.umorili;
+
+import android.app.Application;
+
+import com.raizlabs.android.dbflow.config.FlowConfig;
+import com.raizlabs.android.dbflow.config.FlowManager;
+
+
+public class UmoriliAplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FlowManager.init(new FlowConfig.Builder(this).build());
+    }
+}
